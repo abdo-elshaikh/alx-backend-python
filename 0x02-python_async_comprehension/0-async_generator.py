af7b2a73aa5f605802bc948005997c917464296a
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2024-05-07 22:48:23 +0300

    update generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,6 +7,6 @@ from typing import AsyncIterator
 
 async def async_generator(n: int = 10) -> AsyncIterator[float]:
     """Async generator returning `n` random numbers between 0 and 1"""
-    for _ in range(n):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
